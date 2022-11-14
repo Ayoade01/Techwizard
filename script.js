35a62959e5b037f5061405a028754d23cc72b1b7
Ayoade01 <118178083+Ayoade01@users.noreply.github.com>
2022-11-14 20:33:29 +0100

    Nothing new

---
+++ script.js
@@ -0,0 +1,90 @@
+
+                // javascript to make side menu appear  
+
+                
+                  function openNav(){
+                      document.getElementById("sidenav").style.width ="50%";
+                  }
+  
+  
+                  function closeNav(){
+                      document.getElementById("sidenav").style.width ="0%";
+                  }
+  
+  
+  
+             
+  
+  
+  
+  
+                    //  javascript to make side menu appear  end 
+
+
+
+
+
+
+
+
+
+
+		
+              
+				
+				
+        //             function showTime(){
+        //     var date= new Date();
+        //     var h = date.getHours(); //0-23
+        //     var m = date.getMinutes(); //0-59
+        //     var s = date.getSeconds(); //0-59
+        //     var session= "AM"
+            
+        //     if (h == 0) {
+        //             h = 12;
+        //     }
+            
+        //     if (h > 12) {
+        //             h = h-12;
+        //   session= "PM"
+        //     }
+            
+        //     h = (h < 10)? "0" + h : h;
+        //     m = (m < 10)? "0" + m : m;
+        //     s = (s < 10)? "0" + s : s;
+        //     var time = h+":"+m+":"+s+""+session;
+            
+        //     document.getElementById("MyClockDisplay").innerText=time;
+        //     document.getElementById("MyClock Display").textContent=time;
+        //     setTimeout(showTime, 1000);
+        // }
+        
+        // showTime();
+        
+           
+            
+            
+            
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
